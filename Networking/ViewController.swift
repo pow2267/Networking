95b692007b9eb1c41f56f5cef2961a0d9ab05aea
pow2267 <pow2267@gmail.com>
2021-09-06 23:03:31 +0900

    Set DispatchQueue.global() and main thread

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -25,16 +25,30 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         cell.textLabel?.text = friend.name.full
         cell.detailTextLabel?.text = friend.email
+        cell.imageView?.image = nil // nil처리를 해줘야 cell 재활용 할 때 다른 사람의 이미지가 잘못들어가지 않음
         
-        guard let imageUrl: URL = URL(string: friend.picture.thumbnail) else {
-            return cell
+        DispatchQueue.global().async {
+            guard let imageUrl: URL = URL(string: friend.picture.thumbnail) else {
+                return
+            }
+            
+            // Data는 동기 메소드라서 이미지를 불러올 때까지 동작이 멈추게 됨. 그럼 불편하니까 백그라운드 큐에 넣어줌
+            guard let imageData: Data = try? Data(contentsOf: imageUrl) else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                // image를 셋팅하기 전에 사용자가 스크롤을 하면 화면에 보여지는 cell의 index가 달라질 수 있으므로 index를 비교해주는 코드가 필요
+                if let index: IndexPath = tableView.indexPath(for: cell) {
+                    if index.row == indexPath.row {
+                        cell.imageView?.image = UIImage(data: imageData)
+                        // 댓글에서 알려준 방법 (이 메소드가 없으면 실행했을 때 셀을 눌러야만 사진이 보임)
+                        cell.setNeedsLayout()
+                        // cell.layoutIfNeeded() 이건 없어도 동작 함. 왜지?
+                    }
+                }
+            }
         }
-        
-        guard let imageData: Data = try? Data(contentsOf: imageUrl) else {
-            return cell
-        }
-        
-        cell.imageView?.image = UIImage(data: imageData)
         
         return cell
     }
@@ -63,7 +77,11 @@ class ViewController: UIViewController, UITableViewDataSource {
             do {
                 let apiResponse: APIResponse = try JSONDecoder().decode(APIResponse.self, from: data)
                 self.friends = apiResponse.results
-                self.tableView.reloadData()
+                
+                // 이 코드를 제외한 나머지 코드는 모두 백그라운드에서 실행되는 중
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             } catch (let err) {
                 print(err.localizedDescription)
             }
